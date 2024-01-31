9de89c93c5432eb61de96fd18f6625182897a0b0
Elgadisalma <elgadi.salma0@gmail.com>
2024-01-31 15:57:32 +0100

    Update register.blade.php

--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -20,7 +20,8 @@
 
         <div class="h-screen bg-indigo-100 flex justify-center items-center">
             <div class="lg:w-2/5 md:w-1/2 w-2/3">
-                <form class="bg-white p-10 rounded-lg shadow-lg min-w-full" action="{{ route('register') }}" method="POST">
+                <div class="bg-white p-10 rounded-lg shadow-lg min-w-full">
+                <form action="{{ route('register') }}" method="POST">
 
                     @csrf
                     <h1 class="text-center text-2xl mb-6 text-gray-600 font-bold font-sans">Register</h1>
@@ -39,9 +40,11 @@
                     </div>
 
                     <button type="submit" class="w-full mt-6 bg-purple-400 rounded-lg px-4 py-2 text-lg text-white tracking-wide font-semibold font-sans">Register</button>
-                    <a href=""><button class="w-full mt-6 mb-3 bg-indigo-100 rounded-lg px-4 py-2 text-lg text-gray-800 tracking-wide font-semibold font-sans">Login</button></a>
-
+                
                 </form>
+ 
+                    <a href="{{ route('login') }}"><button class="w-full mt-6 mb-3 bg-indigo-100 rounded-lg px-4 py-2 text-lg text-gray-800 tracking-wide font-semibold font-sans">Login</button></a>
+            </div>
             </div>
         </div>
 
